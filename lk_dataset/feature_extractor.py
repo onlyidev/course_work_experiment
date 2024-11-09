13b8c6b6fde44814af979acd5bd94b05f8aceb62
onlyidev <liudas.kasper@gmail.com>
2024-11-09 16:13:57 +0000

    Export to npy

--- lk_dataset/feature_extractor.py
+++ lk_dataset/feature_extractor.py
@@ -3,26 +3,41 @@ import pickle
 import typing
 import os
 import logging
+import numpy as np
 
 class FeatureExtractor:
     def __init__(self):
         self.db = set()
     
-    def parseAPIs(self, path: str) -> None:
+    def parseAPIs(self, path: str) -> typing.List[typing.Tuple[str, str]]:
         pe = lief.parse(path)
+        arr = []
         for lib in pe.imports:
             for func in lib.entries:
+                arr.append((lib.name, func.name))
                 self.db.add((lib.name, func.name))
+        arr.sort()
+        return arr
             
-    def getAPIs(self) -> typing.List[str]:
-        apiList = [api for _,api in self.db]
+    def getAPIs(self, db = None) -> typing.List[str]:
+        db = self.db if db is None else db
+        apiList = [api for _,api in db]
         apiList.sort()
         return apiList
     
-    def save(self, path: str) -> None:
+    def save(self, path: str, obj = None) -> None:
         with open(path, "wb") as f_out:
-            pickle.dump(self.db, f_out)
-        
+            pickle.dump(self if obj is None else obj, f_out)
+
+    def getLatentMatrix(self, path):
+        files = self.loadDir(path)
+        [self.parseAPIs(file) for file in files] # Load to db
+        return np.array([self.__generateLatentVector(file) for file in files])
+
+    def __generateLatentVector(self, path: str):
+        features = self.getAPIs(self.parseAPIs(path))
+        return np.array([x in features for x in self.getAPIs()], dtype=np.int8)
+
     @staticmethod
     def loadDir(path) -> typing.List[str]:
         print(f"Loading files from {path}")
--- testing.py
+++ testing.py
@@ -1,8 +1,12 @@
 from lk_dataset.feature_extractor import FeatureExtractor
+import numpy as np
 
 fe = FeatureExtractor()
 
-l = [fe.parseAPIs(x) for x in fe.loadDir("./lk_dataset/data")]
+# l = [fe.parseAPIs(x) for x in fe.loadDir("./lk_dataset/data")]
 # l = [x for x in fe.loadDir("./lk_dataset/data") if fe.checkValidPE(x)]
 
-print(len(fe.getAPIs()))
+# print(fe.getAPIs())
+m = fe.getLatentMatrix("./lk_dataset/data")
+print(m)
+np.save("malware.npy", m)
